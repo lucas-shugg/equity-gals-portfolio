777b6ce17e6b8b0040cc373e61e1207c2e5f42c6
lucas-shugg <87249835+lucas-shugg@users.noreply.github.com>
2021-12-13 22:55:19 +1100

    Changed the text theme (#5)
    * Changed the text theme
    
    * Missed a File

--- lib/portfolio/description.dart
+++ lib/portfolio/description.dart
@@ -1,4 +1,3 @@
-import 'package:equity_gals_portfolio/portfolio/portfolio_page.dart';
 import 'package:flutter/material.dart';
 
 class Description extends StatelessWidget {
@@ -14,10 +13,7 @@ class Description extends StatelessWidget {
       child: SelectableText(
         "EGP's Growth ETF Portfolio has been constructed as an educational resource.\n The aim is to highlight the importance of diversification, provide examples of ETFs, and to help you understand asset classes and what they mean.",
         textAlign: TextAlign.center,
-        style: Theme.of(context)
-            .textTheme
-            .headline6!
-            .copyWith(fontFamily: 'AvenirLight', color: egpGreen),
+        style: Theme.of(context).textTheme.headline6,
       ),
     );
   }
