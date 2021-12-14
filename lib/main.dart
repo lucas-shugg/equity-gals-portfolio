fc382307a3f9e66b7b11fbdd42a18c5c5ebc3e2d
Ted Marozzi <38032037+ted-marozzi@users.noreply.github.com>
2021-12-14 19:29:58 +1100

    Applied text theme to data table theme (#8)
    Co-authored-by: lucas-shugg <87249835+lucas-shugg@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,20 @@ void main() {
 
 class EgpPortfolio extends StatelessWidget {
   const EgpPortfolio({Key? key}) : super(key: key);
-
+  static const defaultTextStyle =
+      TextStyle(fontFamily: 'AvenirLight.ttf', color: egpGreen);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PORTFOLIO | equity gals',
       theme: ThemeData(
+        dataTableTheme: const DataTableThemeData(
+            headingTextStyle: defaultTextStyle,
+            dataTextStyle: defaultTextStyle),
         canvasColor: const Color.fromRGBO(254, 238, 227, 1),
         textTheme: const TextTheme(
-            bodyText1:
-                TextStyle(fontFamily: 'AvenirLight.ttf', color: egpGreen),
-            headline6:
-                TextStyle(fontFamily: 'AvenirLight.ttf', color: egpGreen)),
-
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+            bodyText1: defaultTextStyle, headline6: defaultTextStyle),
       ),
       home: const PortfolioPage(),
     );
